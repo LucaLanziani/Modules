c3a392732da7e4d7e677689d4f7c724af1e14fbb
Nss <mosca@llmac-2.local>
2011-06-17 13:27:45 +0200

    added base module

---
+++ BASEMODULE/build/CMakeFiles/BASEMODULE.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/BASEMODULE.dir/BASEMODULE.c.o"
+  "libBASEMODULE.pdb"
+  "libBASEMODULE.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/BASEMODULE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
